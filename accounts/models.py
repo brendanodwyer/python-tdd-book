8281d87cb61d91eb4215f8fbfd0bf0e324e8b6af
brendanodwyer <brendan.odwyer@outlook.com>
2021-07-19 15:37:02 -0500

    De-Spiking Accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,35 +1,16 @@
-import sys
+import uuid
 
-from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import BaseUserManager
-from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 
 class Token(models.Model):
     email = models.EmailField()
-    uid = models.CharField(max_length=255)
+    uid = models.CharField(default=uuid.uuid4, max_length=40)
 
 
-class ListUserManager(BaseUserManager):
-    def create_user(self, email):
-        print("creating user", file=sys.stderr)
-        ListUser.objects.create(email=email)
-
-    def create_superuser(self, email, password):
-        self.create_user(email)
-
-
-class ListUser(AbstractBaseUser, PermissionsMixin):
+class User(models.Model):
     email = models.EmailField(primary_key=True)
+    REQUIRED_FIELDS = []
     USERNAME_FIELD = "email"
-
-    objects = ListUserManager()
-
-    @property
-    def is_staff(self):
-        return self.email == "brendan.odwyer@example.com"
-
-    @property
-    def is_active(self):
-        return True
+    is_anonymous = False
+    is_authenticated = True
