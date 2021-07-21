f1ae63db41564d6ae2d2daa8ea601b9ac8791d38
brendanodwyer <brendan.odwyer@outlook.com>
2021-07-21 18:38:46 -0500

    Committing this before vacation

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -1,26 +1,19 @@
 from django.conf import settings
-from django.contrib.auth import BACKEND_SESSION_KEY
-from django.contrib.auth import get_user_model
-from django.contrib.auth import SESSION_KEY
-from django.contrib.sessions.backends.db import SessionStore
 
 from .base import FunctionalTest
-
-User = get_user_model()
+from .management.commands.create_session import create_pre_authenticated_session
+from .server_tools import create_session_on_server
 
 
 class MyListsTest(FunctionalTest):
     def create_pre_authenticated_session(self, email):
-        user = User.objects.create(email=email)
-
-        session = SessionStore()
-        session[SESSION_KEY] = user.pk
-        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
-        session.save()
-
+        if self.staging_server:
+            session_key = create_session_on_server(self.staging_server, email)
+        else:
+            session_key = create_pre_authenticated_session(email)
         self.browser.get(self.live_server_url + "/404_no_suc_url/")
         self.browser.add_cookie(
-            dict(name=settings.SESSION_COOKIE_NAME, value=session.session_key, path="/")
+            dict(name=settings.SESSION_COOKIE_NAME, value=session_key, path="/")
         )
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
